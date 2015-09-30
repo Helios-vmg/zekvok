af5d04191a5a56b019ca4cf8583a9efec996852d
Víctor M. González <victor.gonzalez@nektra.com>
2015-09-30 11:46:37 -0300

    Moved Transactions.*

--- src/System/Transactions.cpp
+++ src/System/Transactions.cpp
@@ -5,10 +5,10 @@ All rights reserved.
 Distributed under a permissive license. See COPYING.txt for details.
 */
 
-#include "stdafx.h"
+#include "..\stdafx.h"
 #include "Transactions.h"
-#include "Utility.h"
-#include "Exception.h"
+#include "..\Utility.h"
+#include "..\Exception.h"
 
 KernelTransaction::KernelTransaction(){
 	this->tx = CreateTransaction(nullptr, 0, 0, 0, 0, 0, nullptr);
