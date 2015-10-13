ad275f7aad15c160f408a049bb8de7b9dfb5e0ac
Víctor M. González <helios.vmg@gmail.com>
2015-10-12 23:40:15 -0300

    Implemented RsaInputFilter.

--- src/RsaFilter.cpp
+++ src/RsaFilter.cpp
@@ -8,10 +8,10 @@ Distributed under a permissive license. See COPYING.txt for details.
 #include "stdafx.h"
 #include "RsaFilter.h"
 
-RsaOutputFilter::RsaOutputFilter(std::ostream &stream, std::vector<std::uint8_t> &public_key): OutputFilter(stream){
+RsaOutputFilter::RsaOutputFilter(std::ostream &stream, const std::vector<std::uint8_t> *public_key): OutputFilter(stream){
 	this->data.reset(new impl);
 	{
-		CryptoPP::StringSource temp((const byte *)&public_key[0], public_key.size(), true);
+		CryptoPP::StringSource temp((const byte *)&(*public_key)[0], public_key->size(), true);
 		this->data->key.Load(temp);
 	}
 	this->data->encryptor.reset(new encryptor_t(this->data->key));
@@ -47,4 +47,36 @@ bool RsaOutputFilter::flush(){
 		this->data->flushed = true;
 	}
 	return OutputFilter::flush();
+}
+
+RsaInputFilter::RsaInputFilter(std::istream &stream, const std::vector<std::uint8_t> *private_key): InputFilter(stream){
+	this->data.reset(new impl);
+	{
+		CryptoPP::StringSource temp((const byte *)&(*private_key)[0], private_key->size(), true);
+		this->data->key.Load(temp);
+	}
+	this->data->decryptor.reset(new decryptor_t(this->data->key));
+	this->data->filter.reset(new filter_t(this->data->prng, *this->data->decryptor));
+}
+
+std::streamsize RsaInputFilter::read(char *s, std::streamsize n){
+	std::streamsize ret = 0;
+	bool bad = false;
+	auto &buffer = this->data->buffer;
+	auto &filter = *this->data->filter;
+	while (1){
+		auto read = this->next_read((char *)buffer, sizeof(buffer));
+		if (read < 0){
+			bad = true;
+			break;
+		}
+		filter.Put(buffer, read);
+		read = filter.Get((byte *)s, n);
+		if (!read)
+			break;
+		s += read;
+		n -= read;
+		ret += read;
+	}
+	return !ret && bad ? -1 : ret;
 }
--- src/RsaFilter.h
+++ src/RsaFilter.h
@@ -25,7 +25,25 @@ class RsaOutputFilter : public OutputFilter{
 	std::shared_ptr<impl> data;
 
 public:
-	RsaOutputFilter(std::ostream &stream, std::vector<std::uint8_t> &public_key);
+	RsaOutputFilter(std::ostream &stream, const std::vector<std::uint8_t> *public_key);
 	std::streamsize write(const char *s, std::streamsize n) override;
 	bool flush() override;
+};
+
+class RsaInputFilter : public InputFilter{
+	typedef CryptoPP::RSAES<CryptoPP::OAEP<CryptoPP::SHA>>::Decryptor decryptor_t;
+	typedef CryptoPP::PK_DecryptorFilter filter_t;
+	struct impl{
+		CryptoPP::RSA::PrivateKey key;
+		CryptoPP::AutoSeededRandomPool prng;
+		std::unique_ptr<decryptor_t> decryptor;
+		std::unique_ptr<filter_t> filter;
+		std::uint8_t buffer[4096];
+	};
+	
+	std::shared_ptr<impl> data;
+
+public:
+	RsaInputFilter(std::istream &, const std::vector<std::uint8_t> *private_key);
+	std::streamsize read(char *s, std::streamsize n) override;
 };
