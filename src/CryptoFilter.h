edf96ef47f887f99087ae099b89d5a666cce7673
Víctor M. González <helios.vmg@gmail.com>
2015-10-24 09:16:40 -0300

    Removed unused declarations.

--- src/CryptoFilter.h
+++ src/CryptoFilter.h
@@ -7,9 +7,6 @@ Distributed under a permissive license. See COPYING.txt for details.
 
 #pragma once
 
-void encrypt_stream(CryptoPP::RandomNumberGenerator &prng, std::ostream &output, std::istream &input, CryptoPP::RSA::PublicKey &pub);
-void decrypt_stream(CryptoPP::RandomNumberGenerator &prng, std::ostream &output, std::istream &input, CryptoPP::RSA::PrivateKey &priv);
-
 enum class Algorithm{
 	Rijndael,
 	Twofish,
